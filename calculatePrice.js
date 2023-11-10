cbce0ff91e98fa8697a0f499fedc9eb33bdb285a
Pavel Bogator <pavel.bogator@tptlive.ee>
2023-11-10 11:00:12 +0200

    calculatePrice: Fix and reformat the code
    Fix the following bugs:
    
    1. Customer aged 21 couldn't buy anything even though they have to
       (according to the reqs).
    2. Fix the logic of incrementing prices of D products (It gave a float
       number).
    3. Fix loyaltyMember calculation: Now the function adds the coefficient
       of the basePrice *= 0.1 to the basePrice. (In the past, it only
       returned the coefficient).

--- calculatePrice.js
+++ calculatePrice.js
@@ -1,52 +1,68 @@
 function applyAgeRestrictions(customerAge, productType) {
-    if (customerAge <= 21) {
-        return false;
-    }
+  if (customerAge < 21) return false;
 
-    if (customerAge >= 21 && customerAge <= 25 && ["C", "D"].includes(productType)) {
-        return false;
-    }
+  if (
+    customerAge >= 21 &&
+    customerAge <= 25 &&
+    ["C", "D"].includes(productType)
+  ) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
-function applyProductPriceRules(basePrice, productType, hasReturns, isLoyaltyMember) {
+function applyProductPriceRules(
+  basePrice,
+  productType,
+  hasReturns,
+  isLoyaltyMember
+) {
+  if (productType === "D") {
+    basePrice += basePrice * 0.2;
+  }
 
-    if (productType === "D") {
-        basePrice *= 1.20;
-    }
+  if (hasReturns) {
+    basePrice += 150;
+  }
 
-    if (hasReturns) {
-        basePrice += 150;
-    }
+  if (isLoyaltyMember) {
+    basePrice -= basePrice *= 0.1;
+  }
 
-    if (isLoyaltyMember) {
-        basePrice *= 0.10;
-    }
-
-    return basePrice;
+  return basePrice;
 }
 
 function generateProductPrice(customerAge) {
-    return customerAge + 15;
+  return customerAge + 15;
 }
 
-function calculateProductPrice(customerAge, productType, hasReturns, isLoyaltyMember) {
-    const MAX_PRODUCT_PRICE = 2000;
+function calculateProductPrice(
+  customerAge,
+  productType,
+  hasReturns,
+  isLoyaltyMember
+) {
+  const MAX_PRODUCT_PRICE = 2000;
 
-    if (!applyAgeRestrictions(customerAge, productType)) {
-        return "Customer does not meet the purchase requirements.";
-    }
-    let basePrice = generateProductPrice(customerAge);
-    console.log(basePrice);
+  if (!applyAgeRestrictions(customerAge, productType)) {
+    return "Customer does not meet the purchase requirements.";
+  }
+  let basePrice = generateProductPrice(customerAge);
+  console.log(basePrice);
 
-    basePrice = applyProductPriceRules(basePrice, productType, hasReturns, isLoyaltyMember);
+  basePrice = applyProductPriceRules(
+    basePrice,
+    productType,
+    hasReturns,
+    isLoyaltyMember
+  );
 
-    if (basePrice > MAX_PRODUCT_PRICE) {
-        return `Maximum price exceeded: $${MAX_PRODUCT_PRICE}`;
-    }
+  if (basePrice > MAX_PRODUCT_PRICE) {
+    return `Maximum price exceeded: $${MAX_PRODUCT_PRICE}`;
+  }
 
-    return basePrice;
+  return basePrice;
 }
 
 exports.calculateProductPrice = calculateProductPrice;
